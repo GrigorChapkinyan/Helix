6f4d73121d4b3cef4495da976d9fc3b5ed26db01
GrigorChapkinyan <algoqor2008@gmail.com>
2022-08-13 08:43:10 +0400

    first commit

---
+++ Helix/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Helix
+//
+//  Created by Grigor Chapkinyan on 13.08.22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Properties
+    
+    var window: UIWindow?
+
+    // MARK: - App Life Cycle
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
